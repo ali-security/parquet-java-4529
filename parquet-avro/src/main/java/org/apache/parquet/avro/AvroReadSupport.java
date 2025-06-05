67b1102821de7f71c22a066d99a258b901f98e32
Tal Samucha <tal.s@sealsecurity.io>
2025-06-05 14:45:02 +0300

    omit tests

--- parquet-avro/src/main/java/org/apache/parquet/avro/AvroReadSupport.java
+++ parquet-avro/src/main/java/org/apache/parquet/avro/AvroReadSupport.java
@@ -20,8 +20,6 @@ package org.apache.parquet.avro;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.hadoop.conf.Configuration;
@@ -64,12 +62,6 @@ public class AvroReadSupport<T> extends ReadSupport<T> {
   public static final boolean READ_INT96_AS_FIXED_DEFAULT = false;
 
   /**
-   * List of the fully qualified class names separated by ',' that may be referenced from the Avro schema by
-   * "java-class" or "java-key-class" and are allowed to be loaded.
-   */
-  public static final String SERIALIZABLE_CLASSES = "parquet.avro.serializable.classes";
-
-  /**
    * @param configuration       a configuration
    * @param requestedProjection the requested projection schema
    * @see org.apache.parquet.avro.AvroParquetInputFormat#setRequestedProjection(org.apache.hadoop.mapreduce.Job, org.apache.avro.Schema)
@@ -89,24 +81,6 @@ public class AvroReadSupport<T> extends ReadSupport<T> {
 
   public static void setAvroDataSupplier(Configuration configuration, Class<? extends AvroDataSupplier> clazz) {
     configuration.set(AVRO_DATA_SUPPLIER, clazz.getName());
-  }
-
-  public static void setSerializableClasses(Configuration configuration, String... classNames) {
-    if (classNames.length == 0) {
-      configuration.set(AvroReadSupport.SERIALIZABLE_CLASSES, null);
-    } else {
-      configuration.set(AvroReadSupport.SERIALIZABLE_CLASSES, String.join(",", classNames));
-    }
-  }
-
-  public static void setSerializableClasses(Configuration configuration, Class<?>... classes) {
-    if (classes.length == 0) {
-      configuration.set(AvroReadSupport.SERIALIZABLE_CLASSES, null);
-    } else {
-      configuration.set(
-          AvroReadSupport.SERIALIZABLE_CLASSES,
-          Stream.of(classes).map(Class::getName).collect(Collectors.joining(",")));
-    }
   }
 
   private GenericData model = null;
